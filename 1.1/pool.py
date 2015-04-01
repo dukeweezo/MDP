3929f7510f9ddfaf6e1bf7cbb818457f3c89e150
Adam Roberts <dukeweezo@gmail.com>
2015-03-31 17:24:24 -0700

    comments

--- 1.1/pool.py
+++ 1.1/pool.py
@@ -1,6 +1,7 @@
 # MDP Incident Monitor App
 
-"""Pool that managers "workers" to do web-scraping; workers currently single-threaded, but may be multi-thread in future. Communication with UI achieved by queue in messenger.py.
+"""Pool that managers "workers" to do web-scraping; workers currently single-threaded, but may be multi-thread in 
+future. Communication with UI achieved by queue in messenger.py.
 """
 
 import os
